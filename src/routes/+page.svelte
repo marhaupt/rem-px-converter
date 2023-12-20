a54680f817fc277f41442ed731b5e379a1b37886
Martin Haupt <martin23haupt@gmail.com>
2023-12-20 22:57:02 +0100

    some styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 
     let inputValue: string = '';
     let transformedOutput: string | undefined;
+    let copied = false;
 
     $: if (inputValue.trim().length > 0) {
         transformedOutput = transformRemToPx(inputValue);
@@ -13,8 +14,9 @@
     const copyOutput = () => {
         if (transformedOutput) {
             navigator.clipboard.writeText(transformedOutput).then(() => {
-                console.log("Copied!")
-            })
+                copied = true;
+                setTimeout(() => copied = false, 80)
+            });
         }
     };
 </script>
@@ -24,7 +26,11 @@
     <textarea bind:value={inputValue} />
     <!-- svelte-ignore a11y-click-events-have-key-events -->
     <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-    <pre on:click={copyOutput}>{transformedOutput ?? 'nothing yet'}</pre>
+    <pre
+        on:click={copyOutput}
+        class:empty={transformedOutput === undefined}
+        class:copied
+    >{transformedOutput ?? 'nothing yet'}</pre>
 </main>
 
 <style>
@@ -33,15 +39,16 @@
         margin-bottom: 20px;
         grid-column: 1 / -1;
         text-align: center;
-        
-        @media(width < 500px) {
+        color: var(--color-pink);
+        text-shadow: 0 0 10px var(--color-pink);
+
+        @media (width < 500px) {
             margin-bottom: 10px;
         }
     }
 
     main {
         max-width: 700px;
-        padding-top: 20px;
         margin-inline: auto;
         display: grid;
         grid-template-columns: repeat(2, minmax(0, 1fr));
@@ -49,21 +56,35 @@
         row-gap: 20px;
         font-family: monospace;
         font-size: 16px;
-        line-height: 1.4;
-
-        @media(width < 500px) {
+        line-height: 1.8;
+        
+        @media (width < 500px) {
             grid-template-columns: minmax(0, 1fr);
         }
     }
 
     textarea {
+        position: relative;
         resize: vertical;
         field-sizing: content;
-        min-height: 4lh;
-        max-height: 8lh;
+        min-height: 5lh;
+        max-height: 10lh;
         padding: 10px 18px;
         font-size: inherit;
         line-height: inherit;
+        background-color: var(--color-gray-dark);
+        border: 2px solid var(--color-purple);
+        outline: none;
+        color: var(--color-white);
+        transition: border-color 200ms ease, box-shadow 200ms ease;
+        white-space: nowrap;
+        border-radius: 4px;
+
+        &:focus {
+            border-color: var(--color-pink);
+            box-shadow: 0 2px 16px var(--color-pink-alpha60);
+        }
+
     }
 
     pre {
@@ -71,6 +92,21 @@
         padding-block: 10px;
         max-width: 100%;
         overflow: scroll;
-        cursor: pointer;
+        transition-property: color;
+        transition-timing-function: ease-out;
+        transition-duration: 800ms;
+        
+        &:not(.empty) {
+            cursor: pointer;
+        }
+        
+        &.empty {
+            opacity: 0.5;
+        }
+        
+        &.copied {
+            color: var(--color-pink);
+            transition-duration: 50ms;
+        }
     }
 </style>
